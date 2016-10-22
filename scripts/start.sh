b7cacfbf202c71951be8c56c100fd9fa72b6dedb
Bogdan Vaneev <warchantua@gmail.com>
2016-10-22 22:49:18 +0000

    changed start

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
-MONGODB_CLIENT=true
-SCOREBOARD=true
-DEVPLOY=true
-
-if [ MONGODB_CLIENT ]; then
-	cd ${VAGRANT_PATH}/mongoclient-master/ && 
-	tmux new -s mclient -d "sudo meteor --port 3000"
-	echo "[+] mclient has started"
-fi
-
-if [ DEVPLOY ]; then
-	cd ${VAGRANT_PATH}/scripts/ &&
-	tmux new -s devploy -d devploy
-	echo "[+] devploy has started"
-fi
-
-if [ SCOREBOARD ]; then
-	cd ${VAGRANT_PATH}/api/ && 
-	tmux new -s scoreboard -d "sudo python3 daemon_manager.py -i 300 cache_stats"
-	echo "[+] scoreboard has started"
-fi
+#cd ${VAGRANT_PATH}/mongoclient-master/ && tmux new -s mclient -d "sudo meteor --port 3000"
+#cd ${VAGRANT_PATH}/scripts/ && tmux new -s devploy -d devploy
+service mongodb start
+service nginx start
+${VAGRANT_PATH}/scripts/devploy
+#cd ${VAGRANT_PATH}/api/ && tmux new -s scoreboard -d "sudo python3 daemon_manager.py -i 300 cache_stats"
+python3 ${VAGRANT_PATH}/api/daemon_manager.py -i 300 cache_stats
