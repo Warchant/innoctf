74e2646ba3aea3e51ab8dd12bd1414a5e3177f02
Bogdan Vaneev <warchantua@gmail.com>
2016-10-22 22:57:59 +0000

    fix start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,6 @@
 #cd ${VAGRANT_PATH}/scripts/ && tmux new -s devploy -d devploy
 service mongodb start
 service nginx start
-${VAGRANT_PATH}/scripts/devploy
+/ctf/scripts/devploy
 #cd ${VAGRANT_PATH}/api/ && tmux new -s scoreboard -d "sudo python3 daemon_manager.py -i 300 cache_stats"
 python3 ${VAGRANT_PATH}/api/daemon_manager.py -i 300 cache_stats
