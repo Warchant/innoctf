1a00cdb4e6655cd8b16db3cad8fb7118cc7db8a8
Bogdan Vaneev <warchantua@gmail.com>
2016-11-05 16:47:53 +0300

    Added ci

--- platform/scripts/deploy.sh
+++ platform/scripts/deploy.sh
@@ -4,20 +4,15 @@ echo "[+] Running deploy.sh"
 
 # Transpile the CoffeeScript files
 echo "Transpiling Coffeescript"
-coffee -c -o ${VAGRANT_PATH}/web/js/ ${VAGRANT_PATH}/web/coffee/
+coffee -w -c -o ${VAGRANT_PATH}/web/js/ ${VAGRANT_PATH}/web/coffee/
 
 # Clean out the old files
-echo "Cleaning up old files"
-sudo rm -rf /srv/http/ctf/*
+# echo "Cleaning up old files"
+# sudo rm -rf /srv/http/ctf/*
 
 echo "Generating web with Jekyll"
 cd ${VAGRANT_PATH}/web
-sudo jekyll build
-
-echo "Copying files to server"
-mkdir -p ${VAGRANT_PATH}/problem_static
-sudo cp -r ${VAGRANT_PATH}/problem_static /srv/http/ctf/problem-static
+sudo jekyll build --watch
 
 # Make sure everything is in UNIX format.
 sudo dos2unix -q /srv/http/ctf/*.html
-
