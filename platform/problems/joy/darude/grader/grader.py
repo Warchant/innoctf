4d2e8cac2dbbffad46cd44d249004edd4214a560
Aidar Sabirov <audap@mail.ru>
2016-11-07 12:41:36 +0300

    Fixed tasks from Aidar

--- platform/problems/joy/darude/grader/grader.py
+++ platform/problems/joy/darude/grader/grader.py
@@ -1,9 +1,7 @@
 #!/bin/python
 
 def grade(arg, key):
-    if "InnoCTF{sngwwlsik}" == key:
+    if "InnoCTF{sngwwlsik}" in key or "InnoCTF{sngwwlsic}" in key:
         return True, "Correct!"
-	elif "InnoCTF{sngwwlsic}" == key:
-		return True, "Correct!"
     else:
         return False, "Wrong answer, an example of flag is InnoCTF{abcdefghi}"
