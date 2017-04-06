a0bc1e4aaa70102d0886f1bf56a222a4ece1e337
rodionov12 <rodionov12@gmail.com>
2017-04-06 19:34:40 +0300

    add web-iphone

---
+++ platform/problems/web/iphone/grader/grader.py
@@ -0,0 +1,7 @@
+#!/bin/python
+
+def grade(arg, key):
+    if "InnoCTF{1ph0ne7_w45_5ucc35fu11y_b0u6h7}" == key:
+        return True, "Right!"
+    else:
+        return False, "Hmm, maybe some other time?"
